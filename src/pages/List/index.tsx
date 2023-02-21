2fc9125e91ec8c835ec2df560aec86cd72d19355
Carolina Teixeira <maria.silva@creathus.org.br>
2023-02-20 21:41:15 -0400

    feat: cria o componente de List

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
+
+import { Container } from './styles';
+
 const List: React.FC = () => {
+
+  const options = [
+    {value: '2021', label: '2021'},
+    {value: '2022', label: '2022'},
+    {value: '2023', label: '2023'},
+  ];
+
   return (
-    <h1>List</h1>
+    <Container>
+      <ContentHeader title='Saídas' lineColor='#E44C4E'>
+        <SelectInput options={options} />
+      </ ContentHeader>
+    </Container>
   );
 }
 
