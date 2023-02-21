173f76822104e96fd20edc60133cd7feddf91e85
Carolina Teixeira <maria.silva@creathus.org.br>
2023-02-20 21:44:25 -0400

    feat: torna o Header Content dinâmico

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
 
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+
+  const options = [
+    {value: '2021', label: '2021'},
+    {value: '2022', label: '2022'},
+    {value: '2023', label: '2023'},
+  ];
+
   return (
     <Container>
-      <ContentHeader />
+      <ContentHeader title='Dashboard' lineColor='#F7931B'>
+        <SelectInput options={options} />
+      </ ContentHeader>
     </Container> 
   );
 }
