0c69ddd5970edf859d768425463c2e8273df2ebf
Carolina Teixeira <maria.silva@creathus.org.br>
2023-02-21 00:27:06 -0400

    feat: personaliza a scroll bar

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -9,6 +9,20 @@ export const Container = styled.div`
 
     /* mantendo o aside e header fixo na rolagem 
     ** 70px se refere ao header (Layout) */
-    height: calc(100vh -70px);
-    overflow: scroll;
+    height: calc(100vh - 70px);
+    overflow-y: scroll;
+
+    /* personalizando o scroll bar */
+    ::-webkit-scrollbar{
+        width: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb{
+        background-color: ${props => props.theme.colors.secondary};
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-track{
+        background-color: ${props => props.theme.colors.tertiary};
+    }
 `;
