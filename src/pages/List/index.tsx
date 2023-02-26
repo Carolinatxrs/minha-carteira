cf8ae7c59063e74734ea81d6127bcb522221969a
Carolina Teixeira <maria.silva@creathus.org.br>
2023-02-26 00:17:59 -0400

    chore: altera a cor da tag de entradas

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -39,7 +39,7 @@ const List: React.FC = () => {
     return movimentType ==='entry-balance' ?
     {
       title: 'Entradas',
-      lineColor: '#F7931B',
+      lineColor: '#4E41F0',
       data: gains
     } 
     :
