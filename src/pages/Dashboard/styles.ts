0e1a1b79ca6a72691855056afb8b855b13ebad1d
Carolina Teixeira <maria.silva@creathus.org.br>
2023-02-26 22:40:43 -0400

    feat: cria o componente de cartão de mensagem

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.div``;
 
 export const Content = styled.div`
   display: flex;
-
   justify-content: space-between;
+  //joga o box message para linha de baixo
+  flex-wrap: wrap; 
 `;
